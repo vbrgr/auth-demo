8c7c24db6be2d534701bb0f12ab08f2c9ef98385
vbrgr <raju.vallabhani@gmail.com>
2018-08-13 19:32:05 +0530

    commit13agust

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,8 +78,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-/** Add support for window.customElements */
-import '@webcomponents/custom-elements/custom-elements.min';
-
-/** Required for custom elements for apps building to es5. */
-import '@webcomponents/custom-elements/src/native-shim';
