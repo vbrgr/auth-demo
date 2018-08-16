dd0b733c46db27af07cf9da4ae65face08f26940
vbrgr <raju.vallabhani@gmail.com>
2018-08-16 18:57:55 +0530

    commit new

--- server/fetch/fetch-one.js
+++ server/fetch/fetch-one.js
@@ -6,8 +6,8 @@ var router = express.Router();
 var mongoClient = mongodb.MongoClient;
 var url = "mongodb://localhost:27017/products";
 //creat rest API
-router.get("/", (req,res) => {
-  var p_id = req.query.id
+router.get("/:id", (req,res) => {
+  var p_id = req.params.id
   mongoClient.connect(url, (err, db)=> {
     db.collection('products').find({_id : mongojs.ObjectId(p_id)}).toArray((err,data) => {
       res.send(data);
